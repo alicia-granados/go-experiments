c2a128822d98ab21ddf8be1d199871e4949c8a68
alicia-granados <aliciagranados35@gmail.com>
2021-01-22 16:15:56 -0600

    feat: :sparkles: Constantes

---
+++ Constantes/constantes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+const a = 42
+const b = 42.3645
+const c = "Eduardo Tua"
+
+type nombre string
+
+var d nombre
+
+func main() {
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+
+	d = c
+	fmt.Println(d)
+}
