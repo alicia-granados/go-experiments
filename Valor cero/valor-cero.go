3375bb9020a085b88babdf7f7b161f87a43f1e01
alicia-granados <aliciagranados35@gmail.com>
2021-01-22 11:50:05 -0600

    feat: :sparkles: Valor cero

---
+++ Valor cero/valor-cero.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*Declarando la varible con el identificador y de tipo int*/
+var y int
+
+func main() {
+
+	y = 21
+	fmt.Println(y)
+
+}
+
+/* VALORES CERO*
+FALSE para booleanos
+0 para integers
+0.0 para floats
+"" para strings
+nil para poniters,funciones,interfaces,slices,channels, map*/
