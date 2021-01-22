61835f4f688e4c8c95782202fc1d99108519aa90
alicia-granados <aliciagranados35@gmail.com>
2021-01-22 15:53:59 -0600

    feat: :sparkles: Tipo String

---
+++ Tipo String/tipo_String.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	s1 := "Hola Mundo"
+	s2 := `esta es una linea 
+	partida.`
+	fmt.Println(s1)
+	fmt.Printf("El tipo de s1 es: %T\n", s1)
+
+	fmt.Println(s2)
+	fmt.Printf("El tipo de s2 es: %T\n", s2)
+
+	fmt.Println("")
+
+	bs := []byte(s1)
+	fmt.Println(bs)
+	fmt.Printf("El tipo de bs es: %T\n", bs)
+
+	fmt.Println("")
+
+	for i := 0; i < len(s1); i++ { //hexadecimal
+		fmt.Printf("%#U ", s1[i])
+	}
+
+	fmt.Println("")
+
+	for i, v := range s1 { //decimal
+		fmt.Printf("En el índice %d el valor es %v\n", i, v)
+	}
+
+	fmt.Printf("Con el  verbo %q indico que se imprima el string %s", "%s", s1)
+
+}
