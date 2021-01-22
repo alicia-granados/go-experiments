724293003a55df7d7a92c5db9a085ff27cdf01ec
alicia-granados <aliciagranados35@gmail.com>
2021-01-22 11:44:32 -0600

    feat: :sparkles: Operador de Declaración Corta

---
+++ Operador de Declaración corta/Operador-DeclaracionCorta.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*Declaración Corta*/
+	x := 42
+	fmt.Println(x)
+
+	y := 2 + 4
+	fmt.Println(x, y)
+
+	/*Operador de asignación*/
+	x = 5
+	fmt.Println(x, y)
+
+}
