225959db9242f9a797d49881b84e34f4c21c8f7f
alicia-granados <aliciagranados35@gmail.com>
2021-01-26 12:45:50 -0600

    feat: :sparkles: Condicional : Switch

---
+++ Control de flujo/condicional-switch.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch {
+
+	case 2 == 4, 4 == 8, 9 == 9:
+		fmt.Println("No debería imprimir,ahora sí")
+	case 3 == 3:
+		fmt.Println("Debería imprimir")
+		fallthrough
+	case 4 == 5:
+		fmt.Println("No debería imprimir 2")
+	default:
+		fmt.Println("Impriiendo desde el default")
+
+	}
+
+	switch "Manzana" {
+	case "Pera", "Limón":
+		fmt.Println("Frutas verde(s.")
+	case "Manzana", "Ciruela", "Fresas":
+		fmt.Println("Frutas rojas")
+		fallthrough
+	default:
+		fmt.Println("Imprimiendo desde el default")
+	}
+}
