543a146f056540002923ecf87457c7790ccdf73f
alicia-granados <aliciagranados35@gmail.com>
2021-01-28 23:03:57 -0600

    feat: :sparkles: Slice: Eliminando elementos de un slice

---
+++ Agrupando datos/slice_eliminar_elementos.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := []int{4, 5, 46, 47, 8}
+	y := []int{5, 3, 2, 1}
+
+	x = append(x, 7, 8, 96)
+	fmt.Println(x)
+
+	x = append(x, y...)
+	fmt.Println(x)
+
+	x = append(x[:2], x[4:]...)
+	fmt.Println(x)
+
+}
