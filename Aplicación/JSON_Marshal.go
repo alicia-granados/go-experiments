2b44c58121f891dd3af5c818cc4a1768fc9fda5c
alicia-granados <aliciagranados35@gmail.com>
2021-06-16 22:57:13 -0500

    feat: :sparkles: JSON Marshal

---
+++ Aplicación/JSON_Marshal.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type persona struct {
+	Nombre   string
+	Apellido string
+	Edad     int
+}
+
+func main() {
+	p1 := persona{
+		Nombre:   "James",
+		Apellido: "Bond",
+		Edad:     22,
+	}
+	p2 := persona{
+		Nombre:   "Mias",
+		Apellido: "Moneypenny",
+		Edad:     27,
+	}
+	personas := []persona{p1, p2}
+	fmt.Println(personas)
+
+	bs, err := json.Marshal(personas)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
