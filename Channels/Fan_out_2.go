2b8d26d7c0eadef00b6d2357e8394b00b50cecb2
alicia-granados <aliciagranados35@gmail.com>
2021-06-21 20:34:35 -0500

    feat: :sparkles: Fan out

---
+++ Channels/Fan_out_2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	/* Fan oout : consiste en tomar algún trabajo a realizar y convertirlo en varias porciones de trabajp con varias gorutinas */
+
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go agregar(c1)
+	go fanOutIn(c1, c2)
+
+	for v := range c2 {
+		fmt.Println(v)
+	}
+	fmt.Println("Finalizando.")
+}
+
+func agregar(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func fanOutIn(c1, c2 chan int) {
+	var wg sync.WaitGroup
+	const gorutinas = 10
+
+	wg.Add(gorutinas)
+
+	for i := 0; i < gorutinas; i++ {
+		go func() {
+			for v := range c1 {
+				func(v2 int) {
+					c2 <- trabajoConsumeTiempo(v2)
+				}(v)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(c2)
+}
+
+func trabajoConsumeTiempo(n int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	return n + rand.Intn(1000)
+}
