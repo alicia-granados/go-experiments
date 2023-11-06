b66b8b2c26c8ca9db5205d341bf584f7ba482e14
alicia-granados <aliciagranados35@gmail.com>
2023-11-06 16:12:05 -0600

    ✨ feat:operadores aritmeticos y aoquete math

--- Operador de Declaración corta/Operador-DeclaracionCorta.go
+++ Operador de Declaración corta/Operador-DeclaracionCorta.go
@@ -15,4 +15,18 @@ func main() {
 	x = 5
 	fmt.Println(x, y)
 
+	a := 10
+	b := 3
+
+	//operadores de incremento y decremento
+	//a++
+	//b--
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a % b)
+	//operadores por asignacion
+	// a = a+ 5
+	// a += 5
 }
---
+++ Paquete-Math/paquete-Math.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println(math.Pi)
+	fmt.Println(math.E)
+	c := math.Pow(2, 4)
+	fmt.Printf("%.1f\n", c)
+	fmt.Println(math.Cbrt(27))
+
+}
