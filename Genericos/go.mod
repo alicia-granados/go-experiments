baf9412e909f1de960e4595e2eea8efcaaf57671
alicia-granados <aliciagranados35@gmail.com>
2023-11-09 14:31:20 -0600

    ✨ feat:crear restricciones

---
+++ Genericos/go.mod
@@ -0,0 +1,5 @@
+module generic
+
+go 1.21.3
+
+require golang.org/x/exp v0.0.0-20231108232855-2478ac86f678 // indirect
--- Genericos/parametros_Tipos_Resctricciones.go
+++ Genericos/parametros_Tipos_Resctricciones.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints" // resctricciones que ya estan definidas
+)
 
 // restriccion de aproximacion  signo  -> ~ para que no pongan otros tipo de datos
 type integer int
+
+type Numbers interface {
+	~int | ~float64 | ~float32 | ~uint
+}
 
 // funcion variadica con un tipo de dato y resticcion int o float
 func suma[T ~int | ~float64](nums ...T) T {
@@ -15,6 +23,23 @@ func suma[T ~int | ~float64](nums ...T) T {
 	return total
 }
 
+func suma2[T Numbers](nums ...T) T {
+
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+func suma3[T constraints.Integer | constraints.Float](nums ...T) T {
+
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
 func main() {
 	fmt.Println(suma(4, 5, 6, 8))
 	// ya no es necesario poner esto
@@ -24,5 +49,6 @@ func main() {
 	var num1 integer = 100
 	var num2 integer = 200
 	fmt.Println(suma(num1, num2))
-
+	fmt.Println(suma2(num1, num2))
+	fmt.Println(suma3[float32](6.5, 4.3, 50.0))
 }
