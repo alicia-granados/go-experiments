d69b9a5becb88f9f66d189d352c30cece66fc72e
alicia-granados <aliciagranados35@gmail.com>
2023-11-09 12:34:52 -0600

    ✨ feat:Funciones anonimas

--- Funciones/funciones_anonimas.go
+++ Funciones/funciones_anonimas.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 )
 
+func saludar(name string, f func(string)) {
+	f(name)
+}
+
+func duplicar(n int) int {
+	return n * 2
+}
+
+func triplicar(n int) int {
+	return n * 3
+}
+
+func aplicar(f func(int) int, n int) int {
+	return f(n)
+}
+
 func main() {
 	foo()
-
+	// funcion anonima
 	func() {
 		fmt.Println("La función se ejecutó")
 	}()
@@ -14,6 +30,19 @@ func main() {
 	func(x int) {
 		fmt.Println("El significado de la vida es: ", x)
 	}(42)
+
+	saludo := func(name string) {
+		fmt.Printf("Hola, %s\n", name)
+	}
+	saludo("juan")
+
+	saludar("alex", saludo)
+
+	result1 := aplicar(duplicar, 5)
+	result2 := aplicar(triplicar, 4)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
 
 }
 
