f7e6e49a65bacca5867eb7db6c6615907ff95b58
alicia-granados <aliciagranados35@gmail.com>
2023-11-10 17:01:28 -0600

    ✨ feat:crear datos

--- API/main.go
+++ API/main.go
@@ -6,6 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+	ID     string  `json:id`
+	Title  string  `json:title`
+	Artist string  `json:artist`
+	Price  float64 `json:price`
+}
+
+var albums = []album{
+	{ID: "1", Title: "blue train", Artist: "Jhon", Price: 56.99},
+	{ID: "2", Title: "jeru", Artist: "Gerry", Price: 56.99},
+	{ID: "3", Title: "sarah vaughan", Artist: "Sarah", Price: 79.99},
+}
+
 func main() {
 	fmt.Println("Bienvenido a la api")
 
