6f72a4af53923c7d24d567b48045e3dca7acd510
alicia-granados <aliciagranados35@gmail.com>
2023-11-10 16:54:20 -0600

    ✨ feat:instalar gin

--- API/main.go
+++ API/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Bienvenido a la api")
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hola mundo",
+		})
+	})
+
+	router.Run("localhost:8080")
 }
