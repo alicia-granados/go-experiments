4a7dd78a9f22b1bad4fb58754d03cb43ac24d9d5
alicia-granados <aliciagranados35@gmail.com>
2023-11-10 22:25:09 -0600

    ✨ feat:controlador para devolver un elemento en especifico, actualizar y borrar

--- API/main.go
+++ API/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 type album struct {
-	ID     string  `json:id`
-	Title  string  `json:title`
-	Artist string  `json:artist`
-	Price  float64 `json:price`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 var albums = []album{
@@ -36,6 +36,62 @@ func postAlbums(c *gin.Context) {
 	//responder al cliente
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
+
+func getAlbumsID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album no encontrado"})
+}
+
+func deleteAlbumID(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for indx, a := range albums {
+		if a.ID == id {
+			index = indx
+			//c.IndentedJSON(http.StatusOK, a)
+			albums = append(albums[:index], albums[index+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "album eliminado"})
+			//return
+		}
+	}
+	if index == -1 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album no encontrado"})
+	}
+
+}
+
+func patchAlbumID(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for indx, a := range albums {
+		if a.ID == id {
+			index = indx
+
+			var newAlbum album
+			// recibir una estructura -> cargar datos a la esctructura
+			if err := c.BindJSON(&newAlbum); err != nil {
+				return
+			}
+
+			albums[indx] = newAlbum
+			//c.IndentedJSON(http.StatusOK, a)
+
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "album actualizado"})
+			//return
+		}
+	}
+	if index == -1 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album no encontrado"})
+	}
+
+}
 func main() {
 	fmt.Println("Bienvenido a la api")
 
@@ -48,8 +104,10 @@ func main() {
 		})*/
 
 	router.GET("/albums", getAlbums)
-
+	router.GET("/albums/:id", getAlbumsID)
 	router.POST("/albums", postAlbums)
+	router.DELETE("/albums/:id", deleteAlbumID)
+	router.PATCH("/albums/:id", patchAlbumID)
 
 	router.Run("localhost:8081")
 }
