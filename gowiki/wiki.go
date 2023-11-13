1eb167f0b28986a947be9e3f9e9927f82ad6b9c7
alicia-granados <aliciagranados35@gmail.com>
2023-11-12 19:23:00 -0600

    ✨ feat:paquete net/http

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -23,12 +25,23 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hola me encantan los %s", r.URL.Path[1:])
+}
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("Esta es una pagina de muestra")}
+	/*
+		p1 := &Page{Title: "TestPage", Body: []byte("Esta es una pagina de muestra")}
 
-	p1.save()
+		p1.save()
 
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+		p2, _ := loadPage("TestPage")
+		fmt.Println(string(p2.Body))
+	*/
 
+	//responder al cliente con un mensaje
+	http.HandleFunc("/", handler)
+
+	//levantar servidor y regustrar el error
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
