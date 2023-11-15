0b7a496bfae84b55c8651941a80812f0c9b01173
alicia-granados <aliciagranados35@gmail.com>
2023-11-15 11:24:16 -0600

    🧪 test:code coverage

--- Testing/testunitario/mate_test.go
+++ Testing/testunitario/mate_test.go
@@ -30,3 +30,29 @@ func TestSuma2(t *testing.T) {
 		}
 	}
 }
+
+/*coverage : porcentaje del testeo go test-cover
+o crear archivo  go test -coverprofile=coverage.out ,
+ ver que funciones estan cubiertas go tool cover -func=coverage.out
+ go tool cover -html=coverage.out muestra en el html las funciones que ya han sido cubiertas de color verde
+*/
+
+func TestGetMax(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{1, 2, 2},
+		{2, 1, 2},
+		{25, 250, 250},
+	}
+
+	for _, item := range tabla {
+		num_may := GetMax(item.a, item.b)
+
+		if num_may != item.c {
+			t.Errorf("Sel numero mayor , es %d se esperaba %d", num_may, item.c)
+		}
+	}
+}
