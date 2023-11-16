4e44e459663eecd09a0ba94eb5e32fc5111aa024
alicia-granados <aliciagranados35@gmail.com>
2023-11-16 14:20:47 -0600

    ✨ feat:listar registros

--- api-rest/handlers/handlers.go
+++ api-rest/handlers/handlers.go
@@ -1,12 +1,23 @@
 package handlers
 
 import (
+	"api-rest/db"
+	"api-rest/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "lista de todos los usuarios")
+	db.Connect()
+	users := models.ListUser()
+	db.Close()
+	//transformar el objeto a atipo json
+	output, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintln(rw, string(output))
 }
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
--- api-rest/main.go
+++ api-rest/main.go
@@ -18,5 +18,5 @@ func main() {
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	log.Fatal(http.ListenAndServe(":3001", mux))
 }
