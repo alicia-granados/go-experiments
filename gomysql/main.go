3da395bd17a4505e3c1416108b37a82d06bb71aa
alicia-granados <aliciagranados35@gmail.com>
2023-11-16 10:45:53 -0600

    ✨ feat:insertar registro

--- gomysql/main.go
+++ gomysql/main.go
@@ -10,8 +10,11 @@ func main() {
 	db.Connect()
 	//db.Ping()
 	fmt.Println(db.ExistTable("users"))
-	db.CreateTable(models.UserSchema, "users")
+	//db.CreateTable(models.UserSchema, "users")
 
-	db.TruncateTable("users")
+	//db.TruncateTable("users")
+
+	user := models.CreateUser("alex", "alex123", "alex@gmail.com")
+	fmt.Println(user)
 	db.Close()
 }
--- gomysql/models/user.go
+++ gomysql/models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "gomysql/db"
+
 type User struct {
 	Id       int
 	Username string
@@ -13,3 +15,22 @@ const UserSchema string = `CREATE TABLE users(
 	password VARCHAR(100) NOT NULL,
 	email VARCHAR(50),
 	create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+
+// construir ususario
+func NewUser(username, password, email string) *User {
+	user := &User{Username: username, Password: password, Email: email}
+	return user
+}
+
+// Crear usuario e insertar a db
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.insert()
+	return user
+}
+
+// insertar registro metodo privado
+func (user *User) insert() {
+	sql := "INSERT users SET username=?, password= ?, email=?"
+	db.Exec(sql, user.Username, user.Password, user.Email)
+}
