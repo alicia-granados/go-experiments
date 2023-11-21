3bbfecd72e4acec1a5b4223794b21f6da2beaea2
alicia-granados <aliciagranados35@gmail.com>
2023-11-21 14:46:20 -0600

    mejorando nuestro test con pruebas de tablas

--- simple_testing/primeapp/main_test.go
+++ simple_testing/primeapp/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
+	/*result, msg := isPrime(0)
 	if result {
 		t.Errorf("with %d as test parameter, got true but expected false ", 0)
 	}
@@ -17,5 +17,29 @@ func Test_isPrime(t *testing.T) {
 	}
 	if msg != "7 is a prime number!" {
 		t.Error("wrong message returned:", msg)
+	}*/
+
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2"},
 	}
+
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true but got false", e.name)
+		}
+		if !e.expected && result {
+			t.Errorf("%s: expected false but got true", e.name)
+		}
+		if e.msg != msg {
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
+		}
+	}
+
 }
