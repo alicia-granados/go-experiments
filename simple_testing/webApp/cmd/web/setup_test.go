4f6fb515d8f0f4870437ae776a62842744987ce1
alicia-granados <aliciagranados35@gmail.com>
2023-11-29 17:14:12 -0600

    🧪 test:ACTUALIZANDO setup_test.gp para usar el test de repository

--- simple_testing/webApp/cmd/web/setup_test.go
+++ simple_testing/webApp/cmd/web/setup_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"os"
 	"testing"
-	"webapp/pkg/db"
 )
 
 //create an app var of type application
@@ -15,15 +13,8 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
-	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
 
-	conn, err := app.connectToDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	app.DB = db.PostgresConn{DB: conn}
+	app.DB = &dbrepo.TestDBRepo{}
 	os.Exit(m.Run())
 
 }
