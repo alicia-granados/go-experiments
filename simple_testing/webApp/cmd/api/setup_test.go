a9a2ec44aa658ec1b0342305c93c014d36e282f9
alicia-granados <aliciagranados35@gmail.com>
2023-12-07 12:48:37 -0600

    🧪 test:test de generar y validar tokens

--- simple_testing/webApp/cmd/api/setup_test.go
+++ simple_testing/webApp/cmd/api/setup_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var app application
+var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE3MDE2MTE4MTAsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.uX2fL_OMDfh_zJkharhV-R3To2xumYp9y97lI2_PH34"
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}
