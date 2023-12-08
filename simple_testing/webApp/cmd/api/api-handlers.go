c6c8bb17e0fc48316b8cf0df52cebedb89056de6
alicia-granados <aliciagranados35@gmail.com>
2023-12-08 10:52:16 -0600

    ✨ feat:completando el archivo handlers que interactua con el usuario

--- simple_testing/webApp/cmd/api/api-handlers.go
+++ simple_testing/webApp/cmd/api/api-handlers.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"webapp/pkg/data"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -121,21 +123,89 @@ func (app *application) refresh(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {
-
+	// Obtener todos los usuarios de la base de datos utilizando el método AllUsers de la interfaz DB.
+	users, err := app.DB.AllUsers()
+	if err != nil {
+		// Si hay un error al obtener los usuarios, responder con un error JSON y un código de estado 400 (BadRequest).
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	// Si la obtención de usuarios fue exitosa, responder con un JSON que contiene la lista de usuarios y un código de estado 200 (OK).
+	_ = app.writeJSON(w, http.StatusOK, users)
 }
 
 func (app *application) getUser(w http.ResponseWriter, r *http.Request) {
+	// Extraer el parámetro "userID" de la URL y convertirlo a un entero.
+	userID, err := strconv.Atoi(chi.URLParam(r, "userID"))
+	if err != nil {
+		// Si hay un error al convertir el parámetro o si no es un número entero válido, responder con un error JSON y un código de estado HTTP 400 (BadRequest).
+		app.errorJSON(w, err, http.StatusBadRequest)
+	}
 
+	// Obtener el usuario de la base de datos utilizando el ID obtenido.
+	user, err := app.DB.GetUser(userID)
+	if err != nil {
+		// Si hay un error al obtener el usuario, responder con un error JSON y un código de estado HTTP 400 (BadRequest).
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// Si la obtención del usuario fue exitosa, responder con un JSON que contiene la información del usuario y un código de estado HTTP 200 (OK).
+	_ = app.writeJSON(w, http.StatusOK, user)
 }
 
 func (app *application) updateUser(w http.ResponseWriter, r *http.Request) {
 
+	// Declarar una variable para almacenar la información del usuario.
+	var user data.User
+
+	// Leer el JSON del cuerpo de la solicitud y decodificarlo en la estructura de usuario.
+	err := app.readJSON(w, r, &user)
+	if err != nil {
+		// Si hay un error al leer el JSON o decodificarlo, responder con un error JSON y un código de estado HTTP 400 (BadRequest).
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// Llamar al método UpdateUser de la interfaz DB para actualizar la información del usuario en la base de datos.
+	err = app.DB.UpdateUser(user)
+	if err != nil {
+		// Si hay un error al actualizar el usuario en la base de datos, responder con un error JSON y un código de estado HTTP 400 (BadRequest).
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	// Si la actualización del usuario fue exitosa, responder con un código de estado HTTP 204 (NoContent).
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (app *application) deleteUser(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.Atoi(chi.URLParam(r, "userID"))
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+	}
+
+	err = app.DB.DeleteUser(userID)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 
 }
 
 func (app *application) insertUser(w http.ResponseWriter, r *http.Request) {
+	var user data.User
+	err := app.readJSON(w, r, &user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
 
+	_, err = app.DB.InsertUser(user)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
