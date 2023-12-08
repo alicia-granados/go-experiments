d7c1e605f6860ba2f0578222901d353edaef85bc
alicia-granados <aliciagranados35@gmail.com>
2023-12-08 09:47:09 -0600

    🐞 fix: solucion de error en test

--- simple_testing/webApp/cmd/api/api-middleware_test.go
+++ simple_testing/webApp/cmd/api/api-middleware_test.go
@@ -12,26 +12,27 @@ func Test_app_enableCORS(t *testing.T) {
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
 	var tests = []struct {
-		name           string
-		method         string
-		expectedHeader bool
+		name         string
+		method       string
+		expectHeader bool
 	}{
 		{"preflight", "OPTIONS", true},
 		{"get", "GET", false},
 	}
+
 	for _, e := range tests {
 		handlerToTest := app.enableCORS(nextHandler)
 
 		req := httptest.NewRequest(e.method, "http://testing", nil)
-
 		rr := httptest.NewRecorder()
+
 		handlerToTest.ServeHTTP(rr, req)
 
-		if e.expectedHeader && rr.Header().Get("Acces-Control-Allow-Credentials") == "" {
+		if e.expectHeader && rr.Header().Get("Access-Control-Allow-Credentials") == "" {
 			t.Errorf("%s: expected header, but did not find it", e.name)
 		}
 
-		if !e.expectedHeader && rr.Header().Get("Acces-Control-Allow-Credentials") != "" {
+		if !e.expectHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
 			t.Errorf("%s: expected no header, but got one", e.name)
 		}
 	}
