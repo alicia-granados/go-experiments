31628fc6200f34e49b6ac72882cd69ff4491fbf8
alicia-granados <aliciagranados35@gmail.com>
2024-03-26 11:24:07 -0600

    feat: :sparkles: basic of go: packages

---
+++ basicsGo/go.mod
@@ -0,0 +1,3 @@
+module basicsGo
+
+go 1.21.5
--- basicsGo/main.go
+++ basicsGo/main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+var name = "Frontend master"
+
 func main() {
 	fmt.Println("Hello world")
 	print("hello from go")
+
+	printData()
 }
