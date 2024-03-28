9e4d84db3e33d535a859a1240b97ac6252e94152
alicia-granados <aliciagranados35@gmail.com>
2024-03-28 13:22:05 -0600

    feat: :sparkles: basics of go: functions

--- basicsGo/main.go
+++ basicsGo/main.go
@@ -13,6 +13,16 @@ func init() {
 func init() {
 	fmt.Println("B")
 }
+
+func calculateTax(price float32) (float32, float32) {
+	return price * 0.09, price * 0.02
+}
+func calculateTaxWithName(price float32) (stateTax float32, cityTax float32) {
+	stateTax = price * 0.09
+	cityTax = price * 0.02
+	return stateTax, cityTax
+}
+
 func main() {
 	fmt.Println("Hello world")
 	print("hello from go")
@@ -20,4 +30,10 @@ func main() {
 	printData()
 
 	fmt.Println(data.MaxSpeed)
+
+	fmt.Println(calculateTax(100))
+
+	stateTax, cityTax := calculateTax(1000)
+	fmt.Println(stateTax, cityTax)
+	fmt.Println(calculateTaxWithName(300))
 }
