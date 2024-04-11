4b84eafb5a45810d88340a21cc612d9d53d36125
alicia-granados <aliciagranados35@gmail.com>
2024-04-11 11:40:30 -0600

    feat: :sparkles: basics of go: interfaces

--- structMethods/data/course.go
+++ structMethods/data/course.go
@@ -13,6 +13,9 @@ type Course struct {
 	Instructor Instructor
 }
 
+func (c Workshop) SignUp() bool {
+	return true
+}
 func (c Course) String() string {
 	return fmt.Sprintf("---%v---%v", c.Name, c.Instructor.FirstName)
 }
--- structMethods/main.go
+++ structMethods/main.go
@@ -6,16 +6,25 @@ import (
 )
 
 func main() {
+
 	max := data.Instructor{Id: 3, LastName: "Firtman"} // new Instructor
 	max.FirstName = "Maximiliano"
 	fmt.Println(max.Print())
-
-	kyle := data.NewInstructor("Kyle", "Simpson")
-	fmt.Println(kyle.Print())
-
+	/*
+		kyle := data.NewInstructor("Kyle", "Simpson")
+		fmt.Println(kyle.Print())
+	*/
 	goCourse := data.Course{Id: 2, Name: "Go fundamentals", Instructor: max}
 	fmt.Printf("%v", goCourse)
 
 	swiftWS := data.NewWorkshop("Swift with ios", max)
 	fmt.Printf("%v", &swiftWS)
+
+	var courses [2]data.Signable
+	//courses[0] = goCourse
+	courses[1] = swiftWS
+
+	for _, course := range courses {
+		fmt.Println(course)
+	}
 }
